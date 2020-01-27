b255cadc504e6db55f3eaaf424be96d0341cf0a2
cona123 <chaoyue19890625@gmail.com>
2020-01-27 01:26:17 -0800

    change main.cpp for item31

--- item31/main.cpp
+++ item31/main.cpp
@@ -1,10 +1,12 @@
 //#include "Person.h"
 #include <iostream>
-#include "PersonImpl.h"
+// #include "handle/person.h"
+// #include "handle/personImpl.h"
+#include "interface/person.h"
 int main()
 {
-    // std::shared_ptr<Person> realperson = Person::create("a", "199002221", "dfdfdf");
-    // std::cout << realperson->name();
-    std::shared_ptr<Person> person  = std::make_shared<Person>("a", "199002221", "dfdfdf");
-    std::cout << person->name();
+    std::shared_ptr<Person> realperson = Person::create("a", "199002221", "dfdfdf");
+    std::cout << realperson->name();
+    // std::shared_ptr<Person> person  = std::make_shared<Person>("a", "199002221", "dfdfdf");
+    // std::cout << person->name();
 }
