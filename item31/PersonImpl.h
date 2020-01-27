3a9f31b4f69427e529031a201db6b823ca9288b7
cona123 <chaoyue19890625@gmail.com>
2020-01-27 00:27:08 -0800

    fix impl bugs

--- item31/PersonImpl.h
+++ item31/PersonImpl.h
@@ -18,24 +18,24 @@ class Person {
         std::shared_ptr<PersonImpl> pImpl;
 };
 
-class PersonImpl {
-    public:
-        PersonImpl(const std::string& name, const std::string& birthday, 
-        const std::string& addr): name_(name), birthDate_(birthday), address_(addr) {
+// class PersonImpl {
+//     public:
+//         PersonImpl(const std::string& name, const std::string& birthday, 
+//         const std::string& addr): name_(name), birthDate_(birthday), address_(addr) {
 
-        }
-        std::string name() const {
-            return name_;
-        }
-        std::string birthDate() const {
-            return birthDate_;
-        };
-        std::string address() const {
-            return address_;
-        }
-    private:
-        std::string name_;
-        std::string birthDate_;
-        std::string address_;
-};
+//         }
+//         std::string name() const {
+//             return name_;
+//         }
+//         std::string birthDate() const {
+//             return birthDate_;
+//         };
+//         std::string address() const {
+//             return address_;
+//         }
+//     private:
+//         std::string name_;
+//         std::string birthDate_;
+//         std::string address_;
+// };
 #endif
